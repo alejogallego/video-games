53fc55823c4f0301ade857088ae5224492235dea
Alejandro Gallego <alejandroxgallego@gmail.com>
2023-10-04 23:13:45 -0500

    add query

--- data/src/main/java/co/videogames/data/local/VideoJuegosDao.kt
+++ data/src/main/java/co/videogames/data/local/VideoJuegosDao.kt
@@ -12,20 +12,7 @@ interface VideoJuegosDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun guardarVideoJuegoBasedatos(datos: VideoJuegosEntity)
 
-  //  @Update
-   // suspend fun updateInfoToAomTable(aomEntity: VideoJuegosEntity)
-
- //   @Query("SELECT * FROM VIDEO_JUEGOS")
-  //  suspend fun getInfoFromAomTable(): List<VideoJuegosEntity>
-
-  //  @Query("SELECT * FROM VIDEO_JUEGOS WHERE aomSquad = :aomSquad")
-   // suspend fun getInfoFromAomTableBySquad(aomSquad: Int): List<AomEntity>
-
-   // @Query("DELETE FROM VIDEO_JUEGOS WHERE aomKey = :aomKey")
-  //  suspend fun deleteItemByKeyAomTable(aomKey: String)
-
-   //// @Query("SELECT * FROM VIDEO_JUEGOS WHERE aomKey = :aomKey LIMIT 1")
-    //suspend fun getAomByAomKey(aomKey: String): AomEntity
-
+    @Query("SELECT * FROM VIDEO_JUEGOS")
+    suspend fun traerTodaLaListaVideoJuegosBasedatos(): List<VideoJuegosEntity>
 
 }
